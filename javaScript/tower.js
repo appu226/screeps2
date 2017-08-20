f159d3bee7fcfbe0c0d25e1e8bb6e83d6d167f4e
parakram <appu226@yahoo.co.in>
2017-08-20 13:44:04 +0530

    Tower repairs all structures

--- javaScript/tower.js
+++ javaScript/tower.js
@@ -30,103 +30,15 @@ var TowerWrapper = (function () {
         var fullNess = t.energy / t.energyCapacity;
         if (fullNess < .75)
             return;
-        var mem = pv.getTowerMemory(t.id);
-        var target = pv.game.getObjectById(mem.target);
-        if (target === undefined || target == null)
-            mem.status = "free";
-        if (mem.status == "free")
-            this.resetMemStatus(mem, pv);
-        switch (mem.status) {
-            case "free": break;
-            case "other":
-                this.repairOther(mem, pv);
-                break;
-            case "wall":
-                this.repairWallOrRampart(mem, pv);
-                break;
-            case "rampart":
-                this.repairWallOrRampart(mem, pv);
-                break;
-            default: break;
+        var structures = pv.getMyStructuresByRoom(t.room).map(function (sw) { return sw.structure; }).filter(function (s) { return s.hits < s.hitsMax; });
+        var ss = o.maxBy(structures, closestAndWeakestFinder);
+        if (ss.isPresent) {
+            t.repair(ss.get.elem);
         }
         return;
     };
-    TowerWrapper.prototype.repairOther = function (mem, pv) {
-        var t = this.structure;
-        var target = pv.game.getObjectById(mem.target);
-        if (target === undefined || target == null || target.hits == target.hitsMax) {
-            pv.log.debug("tower/repairOther: setting tower " + t.id + " status to free.");
-            mem.status = "free";
-            return;
-        }
-        t.repair(target);
-    };
-    TowerWrapper.prototype.repairWallOrRampart = function (mem, pv) {
-        var t = this.structure;
-        var hitPoints = pv.getWallHitPoints(t.room);
-        var target = pv.game.getObjectById(mem.target);
-        if (target === undefined || target == null || target.hits == target.hitsMax || target.hits >= hitPoints) {
-            pv.log.debug("tower/repairWallOrRampart: setting tower " + t.id + " status to free.");
-            mem.status = "free";
-            return;
-        }
-        t.repair(target);
-    };
-    TowerWrapper.prototype.resetMemStatus = function (mem, pv) {
-        var t = this.structure;
-        var str = partitionStructures(t.room, pv);
-        var wallHitPoints = pv.getWallHitPoints(t.room);
-        var reset = this.findWeakestStructure(str.ramparts, wallHitPoints, "rampart", mem) ||
-            this.findWeakestStructure(str.walls, wallHitPoints, "wall", mem) ||
-            this.findWeakestStructure(str.roads, 1000000000, "other", mem) ||
-            this.findWeakestStructure(str.others, 1000000000, "other", mem);
-        // if all is well, set the bar higher for wall/rampart health
-        if (!reset && t.room.controller.level >= 2) {
-            pv.setWallHitPoints(t.room, Math.min(wallHitPoints + 1000, RAMPART_HITS_MAX[t.room.controller.level]));
-        }
-        if (reset)
-            pv.log.debug("tower/resetMemStatus: setting tower " + t.id + " status to " + mem.status + ".");
-    };
-    TowerWrapper.prototype.findWeakestStructure = function (str, hitPoints, status, mem) {
-        var weakest = o.maxBy(str, function (s) { return s.hits * -1; });
-        if (weakest.isPresent && weakest.get.elem.hits < weakest.get.elem.hitsMax && weakest.get.elem.hits < hitPoints) {
-            mem.status = status;
-            mem.target = weakest.get.elem.id;
-            return true;
-        }
-        return false;
-    };
     return TowerWrapper;
 }());
-function partitionStructures(room, pv) {
-    var str = pv.getMyStructures();
-    var res = {
-        walls: [],
-        ramparts: [],
-        roads: [],
-        others: []
-    };
-    for (var i = 0; i < str.length; ++i) {
-        var sw = str[i];
-        if (!sw.my)
-            continue;
-        switch (sw.structure.structureType) {
-            case STRUCTURE_WALL:
-                res.walls.push(sw.structure);
-                break;
-            case STRUCTURE_ROAD:
-                res.roads.push(sw.structure);
-                break;
-            case STRUCTURE_RAMPART:
-                res.ramparts.push(sw.structure);
-                break;
-            default:
-                res.others.push(sw.structure);
-                break;
-        }
-    }
-    return res;
-}
 function makeTowerWrapper(tower) {
     return new TowerWrapper(tower);
 }
--- src/tower.ts
+++ src/tower.ts
@@ -14,7 +14,7 @@ class TowerWrapper implements StructureWrapper {
 
         let t = this.structure;
         let closestAndWeakestFinder =
-            function (c: Creep): number {
+            function (c: Creep | Structure): number {
                 return 1.0 / mterrain.euclidean(t.pos, c.pos, pv) / c.hits
             };
 
@@ -43,113 +43,17 @@ class TowerWrapper implements StructureWrapper {
         if (fullNess < .75)
             return;
 
-        let mem = pv.getTowerMemory(t.id);
-        let target = pv.game.getObjectById<Structure>(mem.target);
-        if (target === undefined || target == null) mem.status = "free";
-        if (mem.status == "free") this.resetMemStatus(mem, pv);
-        switch (mem.status) {
-            case "free": break;
-            case "other":
-                this.repairOther(mem, pv);
-                break;
-            case "wall":
-                this.repairWallOrRampart(mem, pv);
-                break;
-            case "rampart":
-                this.repairWallOrRampart(mem, pv);
-                break;
-            default: break;
+        let structures =
+            pv.getMyStructuresByRoom(
+                t.room
+            ).map(sw => sw.structure).filter(s => s.hits < s.hitsMax);
+        let ss = o.maxBy<Structure>(structures, closestAndWeakestFinder);
+        if (ss.isPresent) {
+            t.repair(ss.get.elem);
         }
         return;
     }
 
-    repairOther(mem: TowerMemory, pv: Paraverse): void {
-        let t = this.structure;
-        let target = pv.game.getObjectById<Structure>(mem.target);
-        if (target === undefined || target == null || target.hits == target.hitsMax) {
-            pv.log.debug(`tower/repairOther: setting tower ${t.id} status to free.`)
-            mem.status = "free";
-            return;
-        }
-        t.repair(target);
-    }
-
-    repairWallOrRampart(mem: TowerMemory, pv: Paraverse): void {
-        let t = this.structure;
-        let hitPoints = pv.getWallHitPoints(t.room);
-        let target = pv.game.getObjectById<Structure>(mem.target);
-        if (target === undefined || target == null || target.hits == target.hitsMax || target.hits >= hitPoints) {
-            pv.log.debug(`tower/repairWallOrRampart: setting tower ${t.id} status to free.`);
-            mem.status = "free";
-            return;
-        }
-        t.repair(target);
-    }
-
-    resetMemStatus(mem: TowerMemory, pv: Paraverse): void {
-        let t = this.structure;
-        let str = partitionStructures(t.room, pv);
-        let wallHitPoints: number = pv.getWallHitPoints(t.room);
-        let reset = this.findWeakestStructure(str.ramparts, wallHitPoints, "rampart", mem) ||
-            this.findWeakestStructure(str.walls, wallHitPoints, "wall", mem) ||
-            this.findWeakestStructure(str.roads, 1000000000, "other", mem) ||
-            this.findWeakestStructure(str.others, 1000000000, "other", mem);
-        // if all is well, set the bar higher for wall/rampart health
-        if (!reset && t.room.controller.level >= 2) {
-            pv.setWallHitPoints(t.room, Math.min(wallHitPoints + 1000, RAMPART_HITS_MAX[t.room.controller.level]));
-        }
-
-        if (reset)
-            pv.log.debug(`tower/resetMemStatus: setting tower ${t.id} status to ${mem.status}.`)
-    }
-
-    findWeakestStructure(str: Structure[], hitPoints: number, status: string, mem: TowerMemory): boolean {
-        let weakest = o.maxBy<Structure>(str, (s: Structure) => s.hits * -1);
-        if (weakest.isPresent && weakest.get.elem.hits < weakest.get.elem.hitsMax && weakest.get.elem.hits < hitPoints) {
-            mem.status = status;
-            mem.target = weakest.get.elem.id;
-            return true;
-        }
-        return false;
-    }
-
-
-}
-
-function partitionStructures(room: Room, pv: Paraverse): StructurePartition {
-    let str = pv.getMyStructures();
-    let res: StructurePartition = {
-        walls: [],
-        ramparts: [],
-        roads: [],
-        others: []
-    };
-    for (let i = 0; i < str.length; ++i) {
-        let sw = str[i];
-        if (!sw.my) continue;
-        switch (sw.structure.structureType) {
-            case STRUCTURE_WALL:
-                res.walls.push(sw.structure);
-                break;
-            case STRUCTURE_ROAD:
-                res.roads.push(sw.structure);
-                break;
-            case STRUCTURE_RAMPART:
-                res.ramparts.push(sw.structure);
-                break;
-            default:
-                res.others.push(sw.structure);
-                break;
-        }
-    }
-    return res;
-}
-
-interface StructurePartition {
-    walls: Structure[];
-    ramparts: Structure[];
-    roads: Structure[];
-    others: Structure[];
 }
 
 export function makeTowerWrapper(tower: StructureTower): StructureWrapper {
